d32fc172009cff9c6abb18083d066ff620a38e65
Peter Yao <ypeter2212@gmail.com>
2020-12-27 15:33:28 -0500

    Updated some functionality

--- main.py
+++ main.py
@@ -17,11 +17,17 @@ def main() -> None:
         for line in file:
             try:
                 int(line[0])
-                note_date(line, i)
+                note_section(line, i)
                 i += 1
             except:
-                if ord(line[0]) >= 0x4e00 and ord(line[0]) <= 0x9fff:
-                    note_words(line)
+                if ord(line[0]) >= 0x4e00 and ord(line[0]) <= 0x9fff: #Checks if the first character of a line is a Chinese character
+                    count1: int = line.count("-")
+                    count2: int = line.count("–")
+                    count3: int = line.count("—")
+                    if count1 + count2 + count3 != 2:
+                        print("A line with a Chinese word did not contain 3 elements set off by dashes/hyphens and was thus dicarded.")
+                    else:
+                        note_words(line)
     #test_correctness()  #Use this to see if the data was organized correctly.
     show_sections()
     flash_cards()
@@ -31,15 +37,15 @@ def main() -> None:
         
 
 def note_words(line: str) -> None:
-    """Adds a line of text data to the list for words, pinyin, and english."""
+    """Adds a line of text data to the list for Chinese words and two related elements."""
     positions: List[int] = [0, 0, 0, 0, 0, len(line)]
     for i in range(0, len(line)):
-        if line[i] == "-" or line[i] == "–":
+        if line[i] == "-" or line[i] == "–" or line[i] == "—":
             positions[1]= i
             positions[2] = i + 1
             break
     for k in range(positions[0] + 1, len(line)):
-        if line[k] == "-" or line[k] == "–":
+        if line[k] == "-" or line[k] == "–" or line[k] == "—":
             positions[3] = k
             positions[4] = k + 1
     data["words"].append(line[positions[0]: positions[1]].strip())
@@ -47,7 +53,7 @@ def note_words(line: str) -> None:
     data["english"].append(line[positions[4]: positions[5]].strip())
             
 
-def note_date(line: str, sec: int) -> None:
+def note_section(line: str, sec: int) -> None:
     """Processes a line of section data."""
     line = line.replace("\n", "")
     line = "SEC " + str(sec) + ": " + line
@@ -79,6 +85,9 @@ def choose_deck() -> List[int]:
     first: int = 0
     last: int = len(data["words"]) - 1
     print("")
+    if len(secs) == 0 and len(secs_ind) == 0:
+        print("No indicated sections, so all cards will be reviewed.")
+        return [first, last]
     while True:
         sec: int = int(input("Enter the section you want to review (or -1 for all cards): "))
         if sec > len(secs) - 1 or sec < -1:
@@ -90,10 +99,10 @@ def choose_deck() -> List[int]:
                 print("section chosen - all cards")
             break
     if sec != -1 and sec != len(secs_ind) - 1:
-        first = secs_ind[sec] + 1
-        last = secs_ind[sec + 1] - 1
+        first = secs_ind[sec]
+        last = secs_ind[sec + 1]
     elif sec == len(secs_ind) - 1:
-        first = secs_ind[sec] + 1
+        first = secs_ind[sec]
     return [first, last]
 
 
@@ -107,7 +116,6 @@ def flash_cards() -> None:
         if num in secs_ind:
             if reps is False and num not in done_before:
                 done_before.append(num)
-            pass
         elif len(done_before) == section[1] - section[0] + 1 and reps is False:
             print("All cards in deck have been reviewed and repeats are False.")
             while True:
@@ -131,9 +139,7 @@ def flash_cards() -> None:
             done_before.append(num)
             print(data["words"][num])
             entry: str = input("Enter above word, or q to quit, or c to change decks: ")
-            print(data["pinyin"][num])
-            print(data["english"][num])
-            print("")
+            print(f"{data['pinyin'][num]} -- {data['english'][num]}\n")
             if entry == "q" or entry == "Q":
                 print("quitting game...")
                 break
@@ -145,18 +151,33 @@ def flash_cards() -> None:
 
 
 def show_sections() -> None:
+    """Shows the card sections."""
     print("card sections: ")
-    for x in range(0, len(secs)):
-        print(secs[x])
+    if len(secs) == 0 and len(secs_ind) == 0:
+        print("No indicated sections")
+    else:
+        for x in range(0, len(secs)):
+            print(secs[x])
 
 
 def test_correctness() -> None:
+    """Tests if the word data was correctly parsed and flashcards were correctly generated."""
     if len(data["words"]) != len(data["pinyin"]) or len(data["words"]) != len(data["english"]):
-        print("Error: lists storing data for word, pinyin, and english don't match.")
+        print("Error: lists storing word data and related information don't match.")
+    else:
+        print("No word data list mismatch")
+    if len(secs) != len(secs_ind):
+        print("Error: section tracking mismatch.")
+    else:
+        print("No section tracking mismatch")
+    print("The three lists:")
     for x in range(0, len(data["words"])):
         if data['pinyin'][x] == "NEW DECK":
             print("")
         print(f"#{x} {data['words'][x]} -- {data['pinyin'][x]} -- {data['english'][x]}")
+    print("The section trackers:")
+    for i in range(0, len(secs)):
+        print(f"{secs[i]} ----- {secs_ind[i]}")
 
 
 if __name__ == "__main__":
